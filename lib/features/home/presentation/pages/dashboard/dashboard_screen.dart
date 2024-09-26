90dd40d417953e7f7fb9f52b4987549dab51f8c2
apple <sharifsharipov2003@gmail.com>
2024-09-26 11:28:25 +0500

    sidebar and header parts

--- lib/features/home/presentation/pages/dashboard/dashboard_screen.dart
+++ lib/features/home/presentation/pages/dashboard/dashboard_screen.dart
@@ -1,12 +1,15 @@
+import 'package:awesome_extensions/awesome_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:tergov/common/widgets/layouts/templates/site_layout.dart';
-import 'package:tergov/features/home/presentation/pages/dashboard/responsive_screens/dashboard_desktop.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const TSiteTemplate(
+      useLayout: false,
+
+    );
   }
 }
