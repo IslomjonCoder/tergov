4658a10f183e6749955f08c1196940d9109778ec
Nurmuxammad <nurmuhammadzoyidov@gmail.com>
2024-10-01 17:40:39 +0500

    easy localization

---
+++ lib/generated/intl/messages_ru.dart
@@ -0,0 +1,123 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ru locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ru';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "accused": MessageLookupByLibrary.simpleMessage("Обвиняемый"),
+        "add": MessageLookupByLibrary.simpleMessage("Добавить"),
+        "address": MessageLookupByLibrary.simpleMessage("Адресс"),
+        "appName": MessageLookupByLibrary.simpleMessage("Tergov"),
+        "articleExplanation": MessageLookupByLibrary.simpleMessage(
+            "В ходе беседы разъяснено содержание статьи 211 Уголовного кодекса Республики Узбекистан"),
+        "bribery": MessageLookupByLibrary.simpleMessage(
+            "Вымогательство незаконного вознаграждения"),
+        "changeYourPasswordSubTitle": MessageLookupByLibrary.simpleMessage(
+            "Безопасность вашего аккаунта – наш приоритет! Мы отправили вам безопасную ссылку для смены пароля и защиты вашего аккаунта."),
+        "changeYourPasswordTitle": MessageLookupByLibrary.simpleMessage(
+            "Ссылка для сброса пароля отправлена"),
+        "chiefInvestigator": MessageLookupByLibrary.simpleMessage(
+            "Начальник следственного отделения, проводивший беседу: ФИО"),
+        "createAccount":
+            MessageLookupByLibrary.simpleMessage("Создать аккаунт"),
+        "dashboard": MessageLookupByLibrary.simpleMessage("Панель управления"),
+        "day": MessageLookupByLibrary.simpleMessage("День"),
+        "done": MessageLookupByLibrary.simpleMessage("Готово"),
+        "email": MessageLookupByLibrary.simpleMessage("Эл. почта"),
+        "employeeConductingInquiry": MessageLookupByLibrary.simpleMessage(
+            "Сотрудник, проводивший доследственную проверку, дознание или расследование: ФИО"),
+        "enterYourEmail": MessageLookupByLibrary.simpleMessage(
+            "Введите введите вашу электронную почту и пароль для входа в систему"),
+        "forgotPassword":
+            MessageLookupByLibrary.simpleMessage("Забыли пароль?"),
+        "forgotPasswordSubTitle": MessageLookupByLibrary.simpleMessage(
+            "Введите введите вашу электронную почту и мы отправим вам пароль для входа в систему"),
+        "fri": MessageLookupByLibrary.simpleMessage("Пт"),
+        "fullName": MessageLookupByLibrary.simpleMessage("Фио лица"),
+        "interviewConducted":
+            MessageLookupByLibrary.simpleMessage("Беседа проведена"),
+        "interviewDate": MessageLookupByLibrary.simpleMessage("Дата беседы"),
+        "job": MessageLookupByLibrary.simpleMessage("Профессия"),
+        "keepMeLoggedIn":
+            MessageLookupByLibrary.simpleMessage("Оставаться в системе"),
+        "language": MessageLookupByLibrary.simpleMessage("Язык"),
+        "languageChanged": MessageLookupByLibrary.simpleMessage("Язык изменен"),
+        "legalRepresentative":
+            MessageLookupByLibrary.simpleMessage("Законный представитель"),
+        "logout": MessageLookupByLibrary.simpleMessage("Выход"),
+        "longWaiting": MessageLookupByLibrary.simpleMessage(
+            "Длительные беспричинные ожидания"),
+        "mainInfo": MessageLookupByLibrary.simpleMessage("Основная информация"),
+        "maritalStatus":
+            MessageLookupByLibrary.simpleMessage("Семейное положение"),
+        "menu": MessageLookupByLibrary.simpleMessage("Меню"),
+        "misconductOccurred": MessageLookupByLibrary.simpleMessage(
+            "Имели ли место со стороны сотрудника следующие факты?"),
+        "mon": MessageLookupByLibrary.simpleMessage("Пн"),
+        "name": MessageLookupByLibrary.simpleMessage("Имя"),
+        "next": MessageLookupByLibrary.simpleMessage("Далее"),
+        "notRegistered":
+            MessageLookupByLibrary.simpleMessage("Еще не зарегистрированы?"),
+        "password": MessageLookupByLibrary.simpleMessage("Пароль"),
+        "phoneNumber": MessageLookupByLibrary.simpleMessage("Номер телефона"),
+        "physicalPressure":
+            MessageLookupByLibrary.simpleMessage("Физическое давление"),
+        "processParticipant": MessageLookupByLibrary.simpleMessage(
+            "Участник(ца) процесса, прошедший(ая) беседу: ФИО"),
+        "profession": MessageLookupByLibrary.simpleMessage("Профессия"),
+        "profile": MessageLookupByLibrary.simpleMessage("Профиль"),
+        "psychologicalPressure":
+            MessageLookupByLibrary.simpleMessage("Психологическое давление"),
+        "resendEmail":
+            MessageLookupByLibrary.simpleMessage("Отправить на почту повторно"),
+        "residence": MessageLookupByLibrary.simpleMessage("Место проживания"),
+        "rudeBehavior":
+            MessageLookupByLibrary.simpleMessage("Грубое обращение"),
+        "sat": MessageLookupByLibrary.simpleMessage("Сб"),
+        "search": MessageLookupByLibrary.simpleMessage("Поиск"),
+        "selectLanguage": MessageLookupByLibrary.simpleMessage("Выберите язык"),
+        "selectParticipantStatus": MessageLookupByLibrary.simpleMessage(
+            "Выберите Статус участника(цы) процесса"),
+        "selectStatus": MessageLookupByLibrary.simpleMessage("Выберите статус"),
+        "signIn": MessageLookupByLibrary.simpleMessage("Войти"),
+        "status": MessageLookupByLibrary.simpleMessage("Статус"),
+        "step": MessageLookupByLibrary.simpleMessage("Шаг"),
+        "submissionSuccessful": MessageLookupByLibrary.simpleMessage(
+            "Заявление успешно отправлено"),
+        "submit": MessageLookupByLibrary.simpleMessage("Отправить"),
+        "sun": MessageLookupByLibrary.simpleMessage("Вс"),
+        "suspect": MessageLookupByLibrary.simpleMessage("Подозреваемый"),
+        "thu": MessageLookupByLibrary.simpleMessage("Чт"),
+        "totalStats": MessageLookupByLibrary.simpleMessage("Общая статистика"),
+        "tue": MessageLookupByLibrary.simpleMessage("Вт"),
+        "uploadSignature":
+            MessageLookupByLibrary.simpleMessage("Загрузить подпись"),
+        "user": MessageLookupByLibrary.simpleMessage("Пользователь"),
+        "victim": MessageLookupByLibrary.simpleMessage("Потерпевший"),
+        "videoRecording": MessageLookupByLibrary.simpleMessage(
+            "Процесс собеседования видеозаписью"),
+        "warning": MessageLookupByLibrary.simpleMessage("Предупреждение"),
+        "wed": MessageLookupByLibrary.simpleMessage("Ср"),
+        "week": MessageLookupByLibrary.simpleMessage("Неделя"),
+        "year": MessageLookupByLibrary.simpleMessage("Год"),
+        "yourConsent": MessageLookupByLibrary.simpleMessage("Ваше согласие")
+      };
+}
---
+++ lib/generated/intl/messages_uz.dart
@@ -0,0 +1,122 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a uz locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'uz';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "accused": MessageLookupByLibrary.simpleMessage("Ayblanuvchi"),
+        "add": MessageLookupByLibrary.simpleMessage("Qo\'shish"),
+        "address": MessageLookupByLibrary.simpleMessage("Manzil"),
+        "appName": MessageLookupByLibrary.simpleMessage("Tergov"),
+        "articleExplanation": MessageLookupByLibrary.simpleMessage(
+            "O‘zbekiston Respublikasi Jinoyat Kodeksining 211-moddasi mazmuni tushuntirildi"),
+        "bribery": MessageLookupByLibrary.simpleMessage(
+            "Noqonuniy mukofot talab qilish"),
+        "changeYourPasswordSubTitle": MessageLookupByLibrary.simpleMessage(
+            "Hisobingiz xavfsizligi biz uchun muhim! Parolingizni xavfsiz tarzda o\'zgartirish uchun sizga xavfsiz havola yubordik."),
+        "changeYourPasswordTitle": MessageLookupByLibrary.simpleMessage(
+            "Parolni tiklash havolasi yuborildi"),
+        "chiefInvestigator": MessageLookupByLibrary.simpleMessage(
+            "Suhbatni olib borgan tergov bo‘limi boshlig‘i: F.I.Sh"),
+        "createAccount": MessageLookupByLibrary.simpleMessage("Hisob yaratish"),
+        "dashboard": MessageLookupByLibrary.simpleMessage("Boshqaruv paneli"),
+        "day": MessageLookupByLibrary.simpleMessage("Kun"),
+        "done": MessageLookupByLibrary.simpleMessage("Tayyor"),
+        "email": MessageLookupByLibrary.simpleMessage("Elektron pochta"),
+        "employeeConductingInquiry": MessageLookupByLibrary.simpleMessage(
+            "Dastlabki tergov yoki surishtiruv olib borgan xodim: F.I.Sh"),
+        "enterYourEmail": MessageLookupByLibrary.simpleMessage(
+            "Email hamda parolingizni kiriting"),
+        "forgotPassword":
+            MessageLookupByLibrary.simpleMessage("Parolni unutdingizmi?"),
+        "forgotPasswordSubTitle": MessageLookupByLibrary.simpleMessage(
+            "Elektron pochta manzilini kiriting va biz sizga parolingizni tiklash uchun link yuboramiz."),
+        "fri": MessageLookupByLibrary.simpleMessage("Juma"),
+        "fullName": MessageLookupByLibrary.simpleMessage("F.I.Sh"),
+        "interviewConducted":
+            MessageLookupByLibrary.simpleMessage("Suhbat o‘tkazildi"),
+        "interviewDate": MessageLookupByLibrary.simpleMessage("Suhbat sanasi"),
+        "job": MessageLookupByLibrary.simpleMessage("Kasbi"),
+        "keepMeLoggedIn":
+            MessageLookupByLibrary.simpleMessage("Tizimda qolish"),
+        "language": MessageLookupByLibrary.simpleMessage("Til"),
+        "languageChanged":
+            MessageLookupByLibrary.simpleMessage("Til o\'zgartirildi"),
+        "legalRepresentative":
+            MessageLookupByLibrary.simpleMessage("Qonuniy vakil"),
+        "logout": MessageLookupByLibrary.simpleMessage("Chiqish"),
+        "longWaiting":
+            MessageLookupByLibrary.simpleMessage("Asossiz uzoq kutish"),
+        "mainInfo": MessageLookupByLibrary.simpleMessage("Asosiy ma\'lumotlar"),
+        "maritalStatus": MessageLookupByLibrary.simpleMessage("Oilaviy holat"),
+        "menu": MessageLookupByLibrary.simpleMessage("Menyu"),
+        "misconductOccurred": MessageLookupByLibrary.simpleMessage(
+            "Xodim tomonidan quyidagi holatlar yuz berganmi?"),
+        "mon": MessageLookupByLibrary.simpleMessage("Dush"),
+        "name": MessageLookupByLibrary.simpleMessage("Isim"),
+        "next": MessageLookupByLibrary.simpleMessage("Keyingi"),
+        "notRegistered":
+            MessageLookupByLibrary.simpleMessage("Ro\'yxatdan o\'tmaganmisiz?"),
+        "notification": MessageLookupByLibrary.simpleMessage("Bildirishmalar"),
+        "password": MessageLookupByLibrary.simpleMessage("Parol"),
+        "phoneNumber": MessageLookupByLibrary.simpleMessage("Telefon raqami"),
+        "physicalPressure":
+            MessageLookupByLibrary.simpleMessage("Jismoniy bosim"),
+        "processParticipant": MessageLookupByLibrary.simpleMessage(
+            "Jarayon ishtirokchisi, suhbatdan o\'tgan: F.I.Sh"),
+        "profession": MessageLookupByLibrary.simpleMessage("Kasb"),
+        "profile": MessageLookupByLibrary.simpleMessage("Profil"),
+        "psychologicalPressure":
+            MessageLookupByLibrary.simpleMessage("Psixologik bosim"),
+        "resendEmail":
+            MessageLookupByLibrary.simpleMessage("E-mailga qayta yuborish"),
+        "residence": MessageLookupByLibrary.simpleMessage("Yashash joyi"),
+        "rudeBehavior":
+            MessageLookupByLibrary.simpleMessage("Qo‘pol munosabat"),
+        "sat": MessageLookupByLibrary.simpleMessage("Shanba"),
+        "search": MessageLookupByLibrary.simpleMessage("Qidirish"),
+        "selectLanguage": MessageLookupByLibrary.simpleMessage("Tilni tanlang"),
+        "selectParticipantStatus": MessageLookupByLibrary.simpleMessage(
+            "Ishtirokchi maqomini tanlang"),
+        "selectStatus": MessageLookupByLibrary.simpleMessage("Holatni tanlang"),
+        "signIn": MessageLookupByLibrary.simpleMessage("Kirish"),
+        "status": MessageLookupByLibrary.simpleMessage("Holat"),
+        "step": MessageLookupByLibrary.simpleMessage("Qadam"),
+        "submissionSuccessful": MessageLookupByLibrary.simpleMessage(
+            "Ariza muvaffaqiyatli yuborildi"),
+        "submit": MessageLookupByLibrary.simpleMessage("Yuborish"),
+        "sun": MessageLookupByLibrary.simpleMessage("Yakshanba"),
+        "suspect": MessageLookupByLibrary.simpleMessage("Gumondor"),
+        "thu": MessageLookupByLibrary.simpleMessage("Pay"),
+        "totalStats": MessageLookupByLibrary.simpleMessage("Umumiy statistika"),
+        "tue": MessageLookupByLibrary.simpleMessage("Sesh"),
+        "uploadSignature": MessageLookupByLibrary.simpleMessage("Imzo yuklash"),
+        "user": MessageLookupByLibrary.simpleMessage("Foydalanuvchi"),
+        "victim": MessageLookupByLibrary.simpleMessage("Jabrlanuvchi"),
+        "videoRecording": MessageLookupByLibrary.simpleMessage(
+            "Suhbat jarayonining videotasviri"),
+        "warning": MessageLookupByLibrary.simpleMessage("Ogohlantirish"),
+        "wed": MessageLookupByLibrary.simpleMessage("Chor"),
+        "week": MessageLookupByLibrary.simpleMessage("Hafta"),
+        "year": MessageLookupByLibrary.simpleMessage("Yil"),
+        "yourConsent": MessageLookupByLibrary.simpleMessage("Roziligingiz")
+      };
+}
