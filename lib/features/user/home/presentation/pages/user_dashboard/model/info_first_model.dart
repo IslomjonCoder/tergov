2b02cff33a9bcc49f8adf294e95d2928cb346c58
Nurmuxammad <nurmuhammadzoyidov@gmail.com>
2024-10-02 10:50:26 +0500

    model of info

---
+++ lib/features/user/home/presentation/pages/user_dashboard/model/info_first_model.dart
@@ -0,0 +1,59 @@
+class InfoFormFirstModel {
+  String participantFullName;
+  String placeOfResidence;
+  String phoneNumber;
+  String participantStatus;
+  String interviewConducted;
+  String investigatorFullName;
+  String officerFullName;
+  String article211Explanation;
+  String interviewRecorded;
+  DateTime? interviewStartDate;
+  DateTime? interviewEndDate;
+
+  InfoFormFirstModel({
+    this.participantFullName = '',
+    this.placeOfResidence = '',
+    this.phoneNumber = '',
+    this.participantStatus = '',
+    this.interviewConducted = '',
+    this.investigatorFullName = '',
+    this.officerFullName = '',
+    this.article211Explanation = '',
+    this.interviewRecorded = '',
+    this.interviewStartDate,
+    this.interviewEndDate,
+  });
+}
+
+
+extension InfoFormModelCopyWith on InfoFormFirstModel {
+  InfoFormFirstModel copyWith({
+    String? participantFullName,
+    String? placeOfResidence,
+    String? phoneNumber,
+    String? participantStatus,
+    String? interviewConducted,
+    String? investigatorFullName,
+    String? officerFullName,
+    String? article211Explanation,
+    String? interviewRecorded,
+    DateTime? interviewStartDate,
+    DateTime? interviewEndDate,
+  }) {
+    return InfoFormFirstModel(
+      participantFullName: participantFullName ?? this.participantFullName,
+      placeOfResidence: placeOfResidence ?? this.placeOfResidence,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      participantStatus: participantStatus ?? this.participantStatus,
+      interviewConducted: interviewConducted ?? this.interviewConducted,
+      investigatorFullName: investigatorFullName ?? this.investigatorFullName,
+      officerFullName: officerFullName ?? this.officerFullName,
+      article211Explanation: article211Explanation ?? this.article211Explanation,
+      interviewRecorded: interviewRecorded ?? this.interviewRecorded,
+      interviewStartDate: interviewStartDate ?? this.interviewStartDate,
+      interviewEndDate: interviewEndDate ?? this.interviewEndDate,
+    );
+  }
+}
+
--- lib/features/user/home/presentation/pages/user_dashboard/widgets/info_form_1.dart
+++ lib/features/user/home/presentation/pages/user_dashboard/widgets/info_form_1.dart
@@ -5,6 +5,8 @@ import 'package:tergov/features/user/home/presentation/pages/user_dashboard/widg
 import 'package:tergov/features/user/home/presentation/pages/user_dashboard/widgets/warning_message.dart';
 import '../../../../../../../utils/constants/colors.dart';
 import '../../../../cubit/info_form_cubit.dart';
+import '../../../../cubit/info_form_first_cubit.dart';
+import '../model/info_first_model.dart';
 import 'next_form_button.dart';
 
 class InfoFormFirst extends StatelessWidget {
@@ -12,104 +14,69 @@ class InfoFormFirst extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> fields = [
-      {
-        "hintText": "Участник(ца) процесса, прошедший(ая) беседу: ФИО",
-        "keyboardType": TextInputType.text,
-        "hasDropdown": false,
-      },
-      {
-        "hintText": "Место проживания",
-        "keyboardType": TextInputType.text,
-        "hasDropdown": false,
-      },
-      {
-        "hintText": "Номер телефона",
-        "keyboardType": TextInputType.phone,
-        "hasDropdown": false,
-      },
-      {
-        "hintText": "Выберите Статус участника(цы) процесса",
-        "hasDropdown": true,
-        "options": [
-          "Участник",
-          "Собеседник",
-          "Офицер",
-          "Подполковник",
-          "Полковник",
-          "Сержант",
-        ],
-      },
-      {
-        "hintText": "Беседа проведена",
-        "hasDropdown": true,
-        "readOnly": true,
-        "options": ["Да", "Нет"],
-      },
-      {
-        "hintText": "Начальник следственного отделения, проводивший беседу: ФИО",
-        "keyboardType": TextInputType.text,
-        "hasDropdown": false,
-      },
-      {
-        "hintText": "Сотрудник, проводивший доследственную проверку, дознание или расследование: ФИО",
-        "hasDropdown": true,
-        "options": [
-          "Абдуллаев Мухаммадхонович Нуриддин",
-          "Хошимов Мухаммадхонич Азамат",
-        ],
-      },
-      {
-        "hintText": "СВ ходе беседы разъяснено содержание статьи 211 Уголовного кодекса Республики Узбекистан",
-        "hasDropdown": true,
-        "readOnly": true,
-        "options": ["Да", "Нет"],
-      },
-      {
-        "hintText": "Процесс собеседования видеозаписью",
-        "hasDropdown": true,
-        "readOnly": true,
-        "options": ["Да", "Нет"],
-      },
-      {
-        "hintText": "Дата начала беседы",
-        "keyboardType": TextInputType.datetime,
-        "readOnly": true,
-        "hasCalendar": true,
-      },
-      {
-        "hintText": "Дата беседы",
-        "readOnly": true,
-        "hasCalendar": true,
-      },
-    ];
-
-    return BlocBuilder<FormCubit, bool>(
+    return BlocBuilder<InfoFormFirstCubit, InfoFormFirstModel>(
       builder: (context, state) {
+        final List<Map<String, dynamic>> fields = [
+          {
+            "hintText": "Участник(ца) процесса, прошедший(ая) беседу: ФИО",
+            "keyboardType": TextInputType.text,
+            "hasDropdown": false,
+            "value": state.participantFullName,
+            "onChanged": (String value) {
+              context.read<InfoFormFirstCubit>().updateFullName(value);
+            },
+          },
+          {
+            "hintText": "Место проживания",
+            "keyboardType": TextInputType.text,
+            "hasDropdown": false,
+            "value": state.placeOfResidence,
+            "onChanged": (String value) {
+              context.read<InfoFormFirstCubit>().updatePlaceOfResidence(value);
+            },
+          },
+          {
+            "hintText": "Номер телефона",
+            "keyboardType": TextInputType.phone,
+            "hasDropdown": false,
+            "value": state.phoneNumber,
+            "onChanged": (String value) {
+              context.read<InfoFormFirstCubit>().updatePhoneNumber(value);
+            },
+          },
+          {
+            "hintText": "Выберите Статус участника(цы) процесса",
+            "hasDropdown": true,
+            "options": [
+              "Участник",
+              "Собеседник",
+              "Офицер",
+              "Подполковник",
+              "Полковник",
+              "Сержант",
+            ],
+            "value": state.participantStatus,
+            "onChanged": (String value) {
+              context.read<InfoFormFirstCubit>().updateParticipantStatus(value);
+            },
+          },
+          // Add other fields in a similar manner
+        ];
+
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Основная информация",
-              style: TextStyle(
-                fontSize: 32,
-                color: TColors.text006,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            const Gap(20),
-            ...fields.map((field) {
-              return GlobalTextField(
-                hintText: field["hintText"],
-                keyboardType: field["keyboardType"] ?? TextInputType.text,
-                hasDropdown: field["hasDropdown"] ?? false,
-                readOnly: field["readOnly"] ?? false,
-                hasCalendar: field["hasCalendar"] ?? false,
-                options: field["options"] ?? [],
-              );
-            }).toList(),
-
-          ],
+          children: fields.map((field) {
+            return GlobalTextField(
+              hintText: field["hintText"],
+              keyboardType: field["keyboardType"] ?? TextInputType.text,
+              hasDropdown: field["hasDropdown"] ?? false,
+              readOnly: field["readOnly"] ?? false,
+              hasCalendar: field["hasCalendar"] ?? false,
+              options: field["options"] ?? [],
+              initialValue: field["value"],
+              onChanged: field["onChanged"],
+            );
+          }).toList(),
         );
       },
     );
